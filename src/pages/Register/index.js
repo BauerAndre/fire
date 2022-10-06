bd1443c31bdce410722aee5c42783ac0d4ae8a2b
Andre Bauer <andrebauer@live.com>
2022-10-06 09:47:14 +0900

    register page done

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,7 +1,47 @@
+import { useState } from "react";
+
+import { Link } from "react-router-dom";
+
 export default function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleRegister(e) {
+    e.preventDefault();
+    if (email !== "" && password !== "") {
+      alert("test");
+    } else {
+      alert("preencha todo os campos");
+    }
+  }
+
   return (
-    <div>
-      <h1>Pagina Register</h1>
+    <div className="home-container">
+      <h1>Cadastre-se</h1>
+      <span>Vamos criar sua conta!</span>
+
+      <form className="form" onSubmit={handleRegister}>
+        <input
+          type="text"
+          placeholder="Digite seu email..."
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          autoComplete={false}
+          type="password"
+          placeholder="************"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit">Cadastrar</button>
+      </form>
+
+      <Link className="button-link" to="/">
+        Ja possui conta? Faca login!
+      </Link>
     </div>
   );
 }
