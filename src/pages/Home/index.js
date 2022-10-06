d872984dac4ad6db4b0b7a0a9e6ad1d076057c07
Andre Bauer <andrebauer@live.com>
2022-10-06 09:56:43 +0900

    admin page created

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,15 +2,23 @@ import { useState } from "react";
 import "./home.css";
 
 import { Link } from "react-router-dom";
+import { auth } from "../../firebaseConnection";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 export default function Home() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  function handleLogin(e) {
+  async function handleLogin(e) {
     e.preventDefault();
     if (email !== "" && password !== "") {
-      alert("test");
+      await signInWithEmailAndPassword(auth, email, password)
+        .then(() => {
+          //navegar para admin
+        })
+        .catch(() => {
+          console.log("ERRO AO FAZER O LOGIN");
+        });
     } else {
       alert("preencha todo os campos");
     }
