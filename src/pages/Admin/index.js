ad2b8b1e5634f955c96ac98e6320eb05d94f2725
Andre Bauer <andrebauer@live.com>
2022-10-07 11:11:24 +0900

    admin page layout

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,9 +1,39 @@
+import { useState } from "react";
 import "./admin.css";
 
 export default function Admin() {
+  const [tarefaInput, setTarefaInput] = useState("");
+
+  function handleRegister(e) {
+    e.preventDefault();
+
+    alert("clicou");
+  }
+
   return (
-    <div>
-      <h1>Pagina ADMIN</h1>
+    <div className="admin-container">
+      <h1>Minhas tarfas</h1>
+
+      <form className="form" onSubmit={handleRegister}>
+        <textarea
+          placeholder="Digite sua tarefa..."
+          value={tarefaInput}
+          onChange={(e) => setTarefaInput(e.target.value)}
+        ></textarea>
+        <button className="btn-register" type="submit">
+          Registrar tarefa
+        </button>
+      </form>
+
+      <article className="list">
+        <p>Estudar javacript e reactjs a noite</p>
+        <div>
+          <button>Editar</button>
+          <button className="btn-delete">Concluir</button>
+        </div>
+      </article>
+
+      <button className="btn-logout">Sair</button>
     </div>
   );
 }
